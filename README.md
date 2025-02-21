de05115ffa29369b8bd479312abce00efc8fa469
Alex Mamaev <sovest21@yandex.ru>
2025-02-21 08:49:31 +0100

    Add task to the repository and define basic structure

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # avanchange_test_task
 AvanChange test task
+
+
+## Installation
+
+## Usage
+
+```bash
+php AvanChange_PHP_Tests.php
+```
---
+++ functions.php
@@ -0,0 +1,21 @@
+<?php
+
+function process_strings($input)
+{
+	return $input;
+}
+
+function sort_objects($data, $age)
+{
+	return $data;
+}
+
+function flatten_array($input)
+{
+	return $input;
+}
+
+function get_permutation_at_index($input, $index)
+{
+	return $input;
+}
